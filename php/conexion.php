ccb569a6e3cf4cf3e2a2e41c6fa767899a9cb9cc
Antonio3108 <47930075+Antonio3108@users.noreply.github.com>
2019-04-16 19:54:28 -0500

    Update conexion.php
    Cambie las propiedades para que todos entremos con el usuario root y la contraseña vacia en los xampp

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
   //declaramos todos los valores de la conexion
   //el orden es: servidor, usuario de la BD, contraseña del usuario, nombre BD
-  $mysqli = new mysqli("localhost", "antonio", "antonio", "erp");
+  $mysqli = new mysqli("localhost", "root", "", "erp");
 
   if (mysqli_connect_errno()) {
     echo "Conexión fallida: " , mysqli_connect_error();
