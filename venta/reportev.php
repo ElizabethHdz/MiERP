1e8a640b78e90101fb4c3a2e18ee3aba18bfaf2f
Antonio3108 <47930075+Antonio3108@users.noreply.github.com>
2019-04-24 17:34:54 -0500

    Reporte de ventas
    Es este archivo se elaboran los reportes de las ventas de todos los articulos, con ventas totales en pesos y compras totalws en pesos

--- venta/reportev.php
+++ venta/reportev.php
@@ -1,11 +1,28 @@
 <?php
   require('../php/conexion.php');
+  $fecha1 = "";
+  $fecha2 = "";
 
-  $sqlP = "SELECT Fecha, COUNT(Cantidad_Articulos) as Ventas FROM venta WHERE Fecha BETWEEN '2019-1-1' AND '2019-3-31' GROUP BY Fecha";//consultamos los tipos de usuario existentes, se usa para el registro
+  if (!isset($_POST['submit'])) {
+    header("Location: formReportes.php");
+  }
+  else {
+    $fecha1 = $_POST['fecha1'];
+    $fecha2 = $_POST['fecha2'];
+  }
+
+  //$sqlP = "SELECT Fecha, COUNT(Cantidad_Articulos) as Ventas FROM venta WHERE Fecha BETWEEN '2019-1-1' AND '2019-4-31' GROUP BY Fecha";//consultamos los tipos de usuario existentes, se usa para el registro
+  $sqlP = "SELECT MONTH(Fecha) as Mes, COUNT(Cantidad_Articulos) as Ventas FROM venta WHERE Fecha BETWEEN '$fecha1' AND '$fecha2' GROUP BY MONTH(Fecha)";
   $resultP = $mysqli->query($sqlP);//ejecutamos la consulta y guardamos
 
-  $sqlT = "SELECT Fecha, Sum(Total) as Total FROM venta WHERE Fecha BETWEEN '2019-1-1' AND '2019-3-31' GROUP BY Fecha";//consultamos los tipos de usuario existentes, se usa para el registro
+  $sqlT = "SELECT MONTH(Fecha) as Fecha, Sum(Total) as Total FROM venta WHERE Fecha BETWEEN '$fecha1' AND '$fecha2' GROUP BY MONTH(Fecha)";//consultamos los tipos de usuario existentes, se usa para el registro
   $resultT = $mysqli->query($sqlT);//ejecutamos la consulta y guardamos
+
+  $sqlT = "SELECT MONTH(Fecha) as Fecha, Sum(Total) as Total FROM compra WHERE Fecha BETWEEN '$fecha1' AND '$fecha2' GROUP BY MONTH(Fecha)";//consultamos los tipos de usuario existentes, se usa para el registro
+  $resultC = $mysqli->query($sqlT);//ejecutamos la consulta y guardamos
+
+  $sqlP = "SELECT P.Nombre, COUNT(Cantidad_Articulos) As Vendidos FROM detalle_venta D, producto P WHERE (P.Id_Producto=D.Id_Producto) GROUP by D.Id_Producto";//consultamos los tipos de usuario existentes, se usa para el registro
+  $resultVP = $mysqli->query($sqlP);//ejecutamos la consulta y guardamos
  ?>
 
  <html>
@@ -16,10 +33,10 @@
        google.charts.setOnLoadCallback(drawChart);
        function drawChart() {
          var data = google.visualization.arrayToDataTable([
-           ['Fecha', 'Ventas por día'],
+           ['Fecha', 'Ventas por Mes'],
            <?php
            while ($row = $resultP->fetch_assoc()) {
-             echo "['Ventas el ".$row['Fecha']."',".$row['Ventas']."],";
+             echo "['Ventas el mes  ".$row['Mes']."',".$row['Ventas']."],";
            }
             ?>
            //['Work',     11],
@@ -33,8 +50,6 @@
          var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
          chart.draw(data, options);
        }
-
-
        //SEGUNDA GRAFICA
        google.charts.load('current', {'packages':['bar']});
        google.charts.setOnLoadCallback(drawStuff);
@@ -52,7 +67,7 @@
         ]);
 
         var options = {
-          width: 800,        
+          width: 800,
           legend: { position: 'none' },
           chart: {
             title: 'Totales de ventas',
@@ -69,10 +84,81 @@
         // Convert the Classic options to Material options.
         chart1.draw(data, google.charts.Bar.convertOptions(options));
       };
+
+
+     </script>
+
+     <script type="text/javascript">
+     google.charts.load('current', {'packages':['bar']});
+     google.charts.setOnLoadCallback(drawStuff);
+
+    function drawStuff() {
+      var data = new google.visualization.arrayToDataTable([
+        ['Fecha', 'Compras totales por mes($)'],
+        <?php
+        while ($row = $resultC->fetch_assoc()) {
+          echo "['".$row['Fecha']."',".$row['Total']."],";
+        }
+         ?>
+        //["Queen's bishop pawn (c4)", 10],
+        //['Other', 3]
+      ]);
+
+      var options = {
+        width: 800,
+        legend: { position: 'none' },
+        chart: {
+          title: 'Totales de compras',
+          subtitle: 'por mes' },
+        axes: {
+          x: {
+            0: { side: 'top', label: 'Fechas'} // Top x-axis.
+          }
+        },
+        bar: { groupWidth: "90%" }
+      };
+
+      var chart2 = new google.charts.Bar(document.getElementById('top_x_div1'));
+      // Convert the Classic options to Material options.
+      chart2.draw(data, google.charts.Bar.convertOptions(options));
+    };
+     </script>
+     <script type="text/javascript">
+     google.charts.load('current', {packages: ['corechart', 'bar']});
+     google.charts.setOnLoadCallback(drawBasic);
+
+
+function drawBasic() {
+    var data = google.visualization.arrayToDataTable([
+      ['Producto', 'Unidades totales vendidas'],
+      <?php
+      while ($row = $resultVP->fetch_assoc()) {
+        echo "['".$row['Nombre']."',".$row['Vendidos']."],";
+      }
+       ?>
+      //['Philadelphia, PA', 1526000, 1517000]
+    ]);
+
+    var options = {
+        title: 'Ventas totales de productos',
+        chartArea: {width: '50%'},
+        hAxis: {
+          title: 'Cantidades vendidas',
+          minValue: 0
+        },
+        vAxis: {
+          title: 'Producto'
+        }
+      };
+    var chart3 = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart3.draw(data, options);
+  }
      </script>
    </head>
    <body>
      <div id="piechart_3d" style="width: 900px; height: 500px;"></div><br><br>
-     <div id="top_x_div" style="width: 800px; height: 600px;"></div>
+     <div id="top_x_div" style="width: 800px; height: 600px;"></div><br>
+     <div id="top_x_div1" style="width: 800px; height: 600px;"></div>
+     <div id="chart_div"></div><br>
    </body>
  </html>
