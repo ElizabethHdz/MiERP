6933680809533800abb97633489c4ca4ca18e246
Antonio3108 <47930075+Antonio3108@users.noreply.github.com>
2019-04-24 17:39:11 -0500

    Create formReportes.php
    Menu que permite seleccionar los reportes

---
+++ venta/formReportes.php
@@ -0,0 +1,39 @@
+<?php
+  require('../php/conexion.php');
+
+  $sql = "SELECT Id_Producto, Nombre FROM producto;";//consultamos los tipos de usuario existentes, se usa para el registro
+  $result = $mysqli->query($sql);//ejecutamos la consulta y guardamos
+?>
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="reportev.php" method="post">
+      <h2>Reporte por fechas</h2><br>
+      <label>Fecha inicio</label>
+      <input type="date" name="fecha1" value="">
+      <label>Fecha de corte</label>
+      <input type="date" name="fecha2" value="">
+      <input type="submit" name="submit" value="Ver reportes">
+    </form>
+    <br><br>
+    <form class="" action="reporteProductos.php" method="post">
+      <h2>Reporte de productos</h2><br>
+      <label>Seleccione el producto que desea consultar</label>
+      <select class="" name="producto">
+        <?php //con la consulta previa de los tipos de usuario, aqui los imprimimos en el combo para que se seleccionen por nombre, y los guardamos por numero
+        while ($row = $result->fetch_assoc()) { ?>
+          <option value="<?php echo $row['Id_Producto']; ?>"><?php echo $row['Nombre']; ?></option>
+        <?php }; ?>
+      </select>
+      <input type="submit" name="submit" value="Ver reporte de producto">
+
+
+    </form>
+
+  </body>
+</html>
